17cd963a327e128459aa81e676ffa1605ca3bb02
pmtriit-91 <pmtri.it91@gmail.com>
2023-06-19 11:29:41 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Button from '~/components/Button';
 
 function App() {
   return (
     <div className="App">
       <p>Hello</p>
-    </div>
+      <Button></Button>
+    </div >
   );
 }
 
